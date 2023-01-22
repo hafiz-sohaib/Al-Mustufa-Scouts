0d560a4afcb6733e4bbab3b31a9776775df80b64
Hafiz Sohaib <syedsohaibnoman@gmail.com>
2023-01-22 19:58:05 +0500

    Al-Mustufa Scouts

--- config/config.js
+++ config/config.js
@@ -5,4 +5,6 @@ mongoose.set("strictQuery", false);
 
 db = mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopology: true})
 
+if (!db) return console.log('Unable to connect');
+
 module.exports = db;
